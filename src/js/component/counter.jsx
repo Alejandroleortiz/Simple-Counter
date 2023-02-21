f52a55abe121e90556f0e56fcf7601ac72efb000
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-21 03:43:01 +0000

    finalizado contador

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -34,13 +34,12 @@ digitOne: propTypes.number,
 let counter = 0;
 
 setInterval(function () {
-const six = Math.floor(counter/100000);
-const five = Math.floor(counter/10000);
-const four = Math.floor(counter/1000);
-const three = Math.floor(counter/100);
-const two = Math.floor(counter/10);
-const one = Math.floor(counter/1);
-console.log(one, two, three, four, five, six);
+const six = Math.floor(counter/100000)%10; // cada variable esta delimitada hasa 9 segun su posicion
+const five = Math.floor(counter/10000)%10;
+const four = Math.floor(counter/1000)%10;
+const three = Math.floor(counter/100)%10;
+const two = Math.floor(counter/10)%10;
+const one = Math.floor(counter/1)%10;
 
 counter ++;
 
@@ -48,7 +47,7 @@ ReactDOM.render(
 	<Counter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>,
 	document.querySelector('#app'));
 
-},1000)
+},1000) // Ejecuta cada 10 segundos 
 
 export default Counter;
 
